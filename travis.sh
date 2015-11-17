d6137a7a8c903352017a92cedd39ba58555472fe
Isaac I.Y. Saito <130s@2000.jukuin.keio.ac.jp>
2015-11-16 17:53:41 -0800

    Add a travis script specific to this repo

---
+++ travis.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Example of "custom" travis test config that is specific to your own repo, in addition to common travis config if any.
+# Whatever custom tests can be defined below.
+
+set -x
+
+echo "travis.sh in 'custom' or 'client' repo is called";
+
+
